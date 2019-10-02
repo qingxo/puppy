de50fad2b0cac0253226411e39a03b53d7abbce5
qingxo <qingxo@sina.com>
2019-10-02 22:41:14 +0800

    1、新增了对静态文件的支持 2、新增了静态文件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as path from 'path';
+import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(express.static(path.join(__dirname, 'public')));
   await app.listen(3001);
 }
 bootstrap();
