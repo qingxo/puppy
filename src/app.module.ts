d7a7529921ac435f190e3354d2733465dd16c18d
qingxo <qingxo@sina.com>
2020-01-20 17:49:14 +0800

    添加中间件文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { CatController } from './cat/cat.controller';
 import { CommonController } from './common/common.controller';
 import { EsbController } from './esb/esb.controller';
-import { CatService } from './cat/cat.service';
 import { CatModule } from './cat/cat.module';
 import { DatabaseModule } from './database/database.module';
-
+import { LoggerMiddleware } from './common/logger.middleware';
 @Module({
   imports: [CatModule, DatabaseModule],
   controllers: [AppController, CommonController, EsbController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('');
+  }
+}
