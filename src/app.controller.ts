e4b7fd5a19473b6873dae112630fdf2a6e2fbe47
qingxo <qingxo@sina.com>
2020-01-20 17:25:21 +0800

    一些controller的基本操作

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Request } from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,25 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('test')
+  getTestInfo(@Req() request: Request): string {
+    return this.appService.getTest();
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log('the id:' + params.id);
+    return `this action returns a #${params.id} cat`;
+  }
+
+  @Get('test/async')
+  async findAsyncTest(): Promise<any[]> {
+    return [];
+  }
+
+  @Get('test/:id')
+  findOne2(@Param('id') id): string {
+    return `This action returns a #${id} cat`;
   }
 }
